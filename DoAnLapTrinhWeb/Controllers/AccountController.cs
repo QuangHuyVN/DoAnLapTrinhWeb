55ae442b66633643e42c309f50a92d32b1b3aeb3
PuLaSunKa <taikhoanrac14789@gmail.com>
2022-06-25 21:56:34 +0700

    Share(up1)

--- DoAnLapTrinhWeb/Controllers/AccountController.cs
+++ DoAnLapTrinhWeb/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace DoAnLapTrinhWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name, PhoneNumber = model.PhoneNumber, Address = model.Address };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
